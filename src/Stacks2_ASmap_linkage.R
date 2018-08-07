86b94487cbb83923bc1ad50791916f42beed2904
Ido Bar <ido.idobar@gmail.com>
2018-08-07 16:33:01 +1000

    ref_stacks2_analysis

--- src/Stacks2_ASmap_linkage.R
+++ src/Stacks2_ASmap_linkage.R
@@ -18,18 +18,20 @@ pacman::p_load(char=package_list)
 options(stringsAsFactors=FALSE)
 
 # Load vcf file and load associated .RData
-vcf_file <- "../Stacks_vcf_files/stacks_M3m4n3_populations.snps.vcf"  # ref_stacks2_populations.snps.vcf
+stacks_name <- "ref_stacks2"
+vcf_file <- recent_file("../Stacks_vcf_files/filtered_VCFs", 
+                        glue::glue("{stacks_name}_populations.+.vcf"))  # ref_stacks2_populations.snps.vcf
 group_markers <- "LG"
 vcf_basename <- tools::file_path_sans_ext(basename(vcf_file))
   
-source("./Stacks2_OneMap_linkage_map/src/vcf_filtration.R")
+source("./Stacks2_linkage_mapping/src/vcf_filtration.R")
 vcf_filtration(vcf_file, miss_rates = seq(0.2, 0.15, -0.05), geno_rate = 0.8)
 
 #### ASMap ####
 # cleaned file to read
 # clean_vcf_file <- "./data/intermediate_files/ref_stacks2_populations.snps.miss10.clean.vcf"
 clean_vcf_basename <- sub(".clean.vcf", "", basename(clean_vcf_file))
-stacks_name <- sub("_populations.snps", "", sub("\\.miss.+", "", clean_vcf_basename))
+# stacks_name <- sub("_populations.snps", "", sub("\\.miss.+", "", clean_vcf_basename))
 # Manually convert to MapMaker format
 clean_vcf <- read_tsv(clean_vcf_file, comment = "##")
 # Subset just the genotypes (also remove unwanted samples, RL and genotype calling meta-information)
@@ -118,7 +120,7 @@ asmap_thres_df %>% filter(marker_num>150) %>% count(map_name)
 
 
 # selected p7 (from range)
-selected_p <- 17
+selected_p <- 20
 asmap <- asmap_thres_list[[selected_p]]
 LogMsg(glue::glue("Selected p.value is: {p_value_range[selected_p]}"))
 LogMsg(glue::glue("Number of markers in each linkage group: {paste(qtl::nmar(asmap), collapse=', ')}"))
@@ -128,7 +130,7 @@ LogMsg(glue::glue("Length of each linkage group: {paste(qtl::chrlen(asmap), coll
 # use function quickEst() to re-estimate the genetic distances 
 qtl::plotMissing(asmap)
 sg <- statGen(asmap, bychr = FALSE, stat.type = "miss")
-mapBC1 <- subset(asmap, ind = sg$miss < sum(qtl::nmar(asmap))*0.2)
+mapBC1 <- subset(asmap, ind = sg$miss < sum(qtl::nmar(asmap))*0.15)
 gc <- genClones(mapBC1, tol = 0.975)
 if ("cgd" %in% names(gc)) {
   mapBC2 <- fixClones(mapBC1, gc$cgd, consensus = FALSE)
@@ -183,13 +185,14 @@ foreach(l=lod_range) %dopar% {
 }
 profileMark(mapRIL5, stat.type = c("seg.dist", "prop", "dxo", "recomb"),
             layout = c(1, 5), type = "l")
-heatMap(mapRIL5, lmax = 30, chr = "L.6")
+heatMap(mapRIL5, lmax = 30, chr = "L.13")
 nmar(mapRIL5)
 chrlen(mapRIL5)
+mapRIL5$geno$L.4
 # Split linkage group and merge with another (based on heatmap)
-mapRIL5_split <- breakCross(mapRIL5, split = list(`L.6` = "660328:86:-"), suffix = "alpha", sep = "")
-mapRIL5_merged <- mergeCross(mapRIL5_split, merge = list(`L.11` = c("L.11", "L.6B"), 
-                             `L.1` = c("L.9", "L.1"), `L.7`=c("L.6A", "L.7")))
+mapRIL5_split <- breakCross(mapRIL5, split = list(`L.4` = "305093:50:+"), suffix = "alpha", sep = "")
+mapRIL5_merged <- mergeCross(mapRIL5_split, merge = list(`L.4` = c("L.4", "L.13"), 
+                             `L.1` = c("L.3","L.2", "L.1")))
 # mapRIL5_merged <- mstmap(mapRIL5_merged, bychr = FALSE, dist.fun = "kosambi", trace = TRUE, p.value = p_value_range[selected_p])
 # Remove markers from L.10,6,7,15
 heatMap(mapRIL5_merged, lmax = 30)
@@ -204,9 +207,18 @@ heatMap(mapRIL5_added, lmax = 30)
 # reconstruct the map (but don't reassess linkage groups - p.value=2)
 mapRIL5b <- mstmap(mapRIL5_added, bychr = TRUE, trace = TRUE, dist.fun = "kosambi",
                    p.value = 2)
+mapRIL5b$geno$L.4$map[350:length(mapRIL5b$geno$L.4$map)]
+mapRIL5b$geno$L.4$map[500:520]
+
+mapRIL5b_split <- breakCross(mapRIL5b, split = list(`L.4` = c("723124:89:-", "186649:35:-")), suffix = "alpha", sep = "")
+mapRIL5b_merged <- mergeCross(mapRIL5b_split, merge = list(`L.4` = c("L.4A", "L.4C"), 
+                                                   `L.5` = c("L.4B","L.5")))
+mapRIL5b <- mstmap(mapRIL5b_merged, bychr = TRUE, trace = TRUE, dist.fun = "kosambi",
+                   p.value = 2)
 # Check for problematic genotypes
 pg1 <- profileGen(mapRIL5b, bychr = FALSE, stat.type = c("xo", "dxo","miss"), 
-                  id = "Genotype", xo.lambda = 14, layout = c(1, 3), lty = 2, cex = 0.7)3
+                  id = "Genotype", xo.lambda = 14, layout = c(1, 3), lty = 2, cex = 0.7)
+
 # remove problematic genotypes
 mapRIL5c <- mstmap(subsetCross(mapRIL5b, ind = !pg1$xo.lambda), bychr = TRUE, trace = TRUE, 
                    dist.fun = "kosambi", p.value = 2)
@@ -217,6 +229,9 @@ mapRIL5d <- pushCross(mapRIL5c, type = "seg.distortion", pars =
                        list(seg.ratio = "70:0:30"))
 mapRIL5e <- mstmap(mapRIL5d, bychr = TRUE, trace = TRUE, dist.fun =
                     "kosambi", p.value = 2)
+mapRIL5e_merged <- mergeCross(mapRIL5d, merge = list(`L.1` = c("L.1", "UL2")))
+mapRIL5e <- mstmap(mapRIL5e_merged, bychr = TRUE, trace = TRUE, dist.fun =
+                     "kosambi", p.value = 2)
 # Check difference in linkage group lengths
 round(chrlen(mapRIL5e) - chrlen(mapRIL5b), 5)
 # Check difference in number of markers 
@@ -227,9 +242,10 @@ heatMap(mapRIL5e, lmax = 30)
 #### Compare linkage map to physical map ####
 unlinked_markers <- sapply(mapRIL5e$geno, function(lg) {if(length(lg$map>10)) return(names(lg$map))})
 marker_map_df <- imap_dfr(mapRIL5e$geno, ~tibble(LG_name = .y, marker_name = markernames(mapRIL5e, .y))) %>%
-  inner_join(clean_vcf[c(1:3)], by = c("marker_name" = "ID"))
+  inner_join(clean_vcf[c(1:3)], by = c("marker_name" = "ID")) %>% 
+  mutate(Chr=sub("_.+", "", `#CHROM`))
 
-marker_map_sum <- marker_map_df %>% group_by(LG_name) %>% summarise(num=n())
+marker_map_sum <- marker_map_df %>% group_by(Chr) %>% summarise(num=n())
 # Manually merge all linkage groups belonging to the same chromosome
 mapRIL5_chrom <- mergeCross(mapRIL5e, merge = list("LcChr1" = c("L.1", "L.8", "L.13"), 
                            "LcChr2" = c("L.10", "L.5", "L.14", "L.15", "L.16"), "LcChr3"=c("L.11", "L.18"),
